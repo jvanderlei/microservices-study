625c0e55486f34cbb8b7754dfe36df1fd6f20762
jvanderlei <79838953+jvanderlei@users.noreply.github.com>
2024-01-28 20:09:26 -0300

    feat: product repository layer
    interface; implementation;

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.API;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetProducts();
+    Task<Product> GetProduct(string id);
+    Task<IEnumerable<Product>> GetProductByName(string name);
+    Task<IEnumerable<Product>> GerProductByCategory(string categoryName);
+
+    Task CreateProduct(Product product);
+    Task<bool> UpdateProduct(Product product);
+    Task<bool> DeleteProduct(string id);
+}
